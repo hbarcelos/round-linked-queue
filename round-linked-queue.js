84720d22b5ee61efc973202322ab26e37b7644f7
Henrique Barcelos <hbarcelos909@gmail.com>
2020-02-04 18:22:13 -0300

    refactor: updated to use ES6 constructions

--- round-linked-queue.js
+++ round-linked-queue.js
@@ -1,88 +1,87 @@
-'use strict';
+"use strict";
 
-var RoundLinkedQueue = function RoundLinkedQueue(maxSize) {
-    this.maxSize = maxSize;
-    this.size = 0;
+class RoundLinkedQueue {
+  static createNode(data, next) {
+    next = next || null;
+    return {
+      data: data,
+      next: next,
+    };
+  }
+
+  constructor(maxLength) {
+    this._maxLength = maxLength;
+    this._length = 0;
     this._root = null;
     this._last = null;
+  }
+
+  get maxLength() {
+    return this._maxLength;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  add(data) {
+    const node = RoundLinkedQueue.createNode(data);
+
+    if (this.length < this._maxLength) {
+      if (this._root === null) {
+        this._last = this._root = node;
+      } else {
+        this._last.next = node;
+        this._last = node;
+      }
+      this._length++;
+    } else {
+      const popped = this._root.data;
+
+      this._root = this._root.next;
+      this._last.next = node;
+      this._last = node;
+
+      return popped;
+    }
+  }
+
+  remove() {
+    if (this.root === null) {
+      throw new Error("Cannot pop from empty queue");
+    }
+
+    const data = this._root.data;
+
+    this._root = this._root.next;
+    this._length--;
+
+    return data;
+  }
+
+  first() {
+    return this._root.data;
+  }
+
+  last() {
+    return this._last.data;
+  }
+
+  toString() {
+    if (this.root === null) {
+      return "<empty>";
+    } else {
+      let next = this.root;
+      let str = "[ ";
+
+      do {
+        str += next.data.toString() + ", ";
+        next = next.next;
+      } while (next !== null);
+
+      return str.slice(0, -2) + " ]";
+    }
+  }
 }
 
 module.exports = RoundLinkedQueue;
-
-RoundLinkedQueue.createNode = function(data, next) {
-    next = next || null;
-    return {
-        data: data,
-        next: next
-    };
-};
-
-RoundLinkedQueue.prototype.push = function(data) {
-    var node = RoundLinkedQueue.createNode(data);
-
-    if (this.size < this.maxSize) {
-        if (this._root === null) {
-            this._last = this._root = node;
-        } else {
-            this._last.next = node;
-            this._last = node;
-        }
-        this.size++;
-    } else {
-        var data = this._root.data;
-
-        this._root = this._root.next;
-        this._last.next = node;
-        this._last = node;
-
-        return data;
-    }
-};
-
-RoundLinkedQueue.prototype.pop = function() {
-    if (this.root === null) {
-        throw new Error('Cannot pop from empty queue');
-    }
-
-    var data = this._root.data;
-    
-    this._root = this._root.next;
-    this.size--;
-
-    return data;
-};
-
-RoundLinkedQueue.prototype.first = function() {
-    return this._root.data;
-};
-
-RoundLinkedQueue.prototype.last = function() {
-    return this._last.data;
-};
-
-
-RoundLinkedQueue.prototype.toString = function() {
-    if (this.root === null) {
-        return '<empty>';
-    } else {
-        var next = this.root;
-        var str = '[ ';
-        do {
-            str += next.data.toString() + ', '; 
-            next = next.next;
-        } while (next !== null);
-        return str.slice(0, -2) + ' ]';
-    }
-};
-
-RoundLinkedQueue.prototype.toArray = function() {
-    var arr = [];
-    
-    var el = this.root;
-    while (el !== null) {
-        arr.push(el.data);
-        el = el.next;
-    }
-    return arr;
-}
-
---
+++ round-linked-queue.test.js
@@ -0,0 +1,68 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const RoundQueue = require("./round-linked-queue");
+
+describe("Round-Queue", () => {
+  describe("When adding elements", () => {
+    it("Should add an element to the end of a non-full queue", () => {
+      // Queue with max length of 3
+      const nonFullQueue = new RoundQueue(3);
+      const originalLength = nonFullQueue.length;
+
+      const poppedElement = nonFullQueue.add(1);
+
+      // When the queue is not full, no popping will be done
+      expect(poppedElement).to.equal(undefined);
+      // Element should've been added to the end of the queue
+      expect(nonFullQueue.last()).to.equal(1);
+      // Length should've been increased by 1
+      expect(nonFullQueue.length).to.equal(originalLength + 1);
+    });
+
+    it("Should pop the first element and add the new element to the end of a full queue", () => {
+      const fullQueue = new RoundQueue(3);
+
+      fullQueue.add(1);
+      fullQueue.add(2);
+      fullQueue.add(3); // Queue is now full
+
+      const originalLength = fullQueue.length;
+      const poppedElement = fullQueue.add(4);
+
+      // The first element should've been popped
+      expect(poppedElement).equal(1);
+      // The second element should've been shifted to the first position
+      expect(fullQueue.first()).to.equal(2);
+      // The new element should've been added to the end of the queue
+      expect(fullQueue.last()).to.equal(4);
+      // The length shouldn't have changed
+      expect(fullQueue.length).to.equal(originalLength);
+    });
+  });
+
+  describe("When removing elements", () => {
+    it("Should remove the first element of a non-empty queue", () => {
+      const nonEmptyQueue = new RoundQueue(3);
+
+      nonEmptyQueue.add(1);
+      nonEmptyQueue.add(2);
+
+      const originalLength = nonEmptyQueue.length;
+      const poppedElement = nonEmptyQueue.remove();
+
+      // The first elements should've been popped
+      expect(poppedElement).to.equal(1);
+      // The second element should've been shifted to the first position
+      expect(nonEmptyQueue.first()).to.equal(2);
+      // The length should've been decreased by 1
+      expect(nonEmptyQueue.length).to.equal(originalLength - 1);
+    });
+
+    it("Should throw an error on an empty queue", function() {
+      const emptyQueue = new RoundQueue(3);
+
+      expect(emptyQueue.remove).to.throw(Error);
+    });
+  });
+});
