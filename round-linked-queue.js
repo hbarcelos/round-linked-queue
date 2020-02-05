c69968e00f0754bfb1ba3c657df3dba7211aadee
Henrique Barcelos <hbarcelos909@gmail.com>
2020-02-05 10:59:32 -0300

    feature: added maxLength property

--- round-linked-queue.js
+++ round-linked-queue.js
@@ -1,6 +1,13 @@
 "use strict";
 
 class RoundLinkedQueue {
+  constructor(maxLength) {
+    this._maxLength = maxLength;
+  }
+
+  get maxLength() {
+    return this._maxLength;
+  }
 }
 
 module.exports = RoundLinkedQueue;
--- round-linked-queue.test.js
+++ round-linked-queue.test.js
@@ -4,4 +4,13 @@ const expect = chai.expect;
 const RoundQueue = require("./round-linked-queue");
 
 describe("Round-Queue", () => {
+  describe("When creating an instance", () => {
+    it("Should properly set the maxLength property", () => {
+      const queueLength = 3;
+
+      const queue = new RoundQueue(queueLength);
+
+      expect(queue.maxLength).to.equal(queueLength);
+    });
+  });
 });
