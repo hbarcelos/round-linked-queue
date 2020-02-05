1d1bddc6dc120e0d622385b34daf67dfb093f28b
Henrique Barcelos <hbarcelos909@gmail.com>
2020-02-05 11:24:49 -0300

    feature: added initial implementation of .add() method

--- round-linked-queue.js
+++ round-linked-queue.js
@@ -4,6 +4,9 @@ class RoundLinkedQueue {
   constructor(maxLength) {
     this._maxLength = maxLength;
     this._length = 0;
+    this._root = 0;
+    this._first = null;
+    this._last = null;
   }
 
   get maxLength() {
@@ -12,6 +15,22 @@ class RoundLinkedQueue {
 
   get length() {
     return this._length;
+  }
+
+  get first() {
+    return this._first;
+  }
+
+  get last() {
+    return this._last;
+  }
+
+  add(element) {
+    this._root = element;
+    this._first = element;
+    this._last = element;
+
+    this._length += 1;
   }
 }
 
--- round-linked-queue.test.js
+++ round-linked-queue.test.js
@@ -21,4 +21,21 @@ describe("Round-Queue", () => {
       expect(queue.length).to.equal(0);
     });
   });
+
+  describe("When adding elements", () => {
+    it("Should add an element to an empty queue", () => {
+      const queue = new RoundQueue(3);
+      const originalLength = queue.length;
+      const elementToAdd = 1;
+
+      queue.add(elementToAdd);
+
+      // Element should've been added to the end of the queue
+      expect(queue.last).to.equal(elementToAdd);
+      // But since it is now the only element, it should also be the at beginning as well
+      expect(queue.first).to.equal(elementToAdd);
+      // Length should've been increased by 1
+      expect(queue.length).to.equal(originalLength + 1);
+    });
+  });
 });
